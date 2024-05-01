ccfffa96bf6ac1e20beacd9a3266591c6688b075
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2024-05-01 22:51:34 +0300

    init: start main page task

--- main_page/views.py
+++ main_page/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
+def main(request):
+    days_of_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    data = {
+        'days': days_of_week
+    }
+    return render(request, 'main_page/main.html', context=data )
