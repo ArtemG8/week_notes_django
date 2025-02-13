31c4825b7be5b7ce39fd2ebc021e31ab843e6de0
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2025-02-13 23:38:25 +0300

    feat: refactor model feat: refactor forms feat: add 404 page

--- weeks_notes/urls.py
+++ weeks_notes/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from main_page import views
+from django.urls import include, path
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_page.urls'))
-]
+] + debug_toolbar_urls()
+
+handler404 = views.page_not_found
