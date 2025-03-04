e4b45318ee881404aa158de6f4d51ec2205628f9
ArtemG8 <120787936+ArtemG8@users.noreply.github.com>
2025-03-04 23:52:37 +0300

    feat: add correct text for form error input messages

--- main_page/forms.py
+++ main_page/forms.py
@@ -16,13 +16,13 @@ class DataForm(forms.Form):
     #                                                  'invalid': 'Что-то пошло не так, повторите запрос.',
     #                                                  }, )
     title = forms.CharField(label='Ваша задача', min_length=3,
-                            error_messages={'required': 'Заполните поля!',
+                            error_messages={'required': 'Введите название задачи',
                                             'max_length': 'Ваше имя не может быть на столько длинным!',
                                             'min_length': 'Ваше имя не может быть на столько коротким!'
                                             })
-    date_of_task = forms.DateTimeField(label='Выберите день задачи', error_messages={'required': 'Заполните поля!'},
+    date_of_task = forms.DateTimeField(label='Выберите день задачи', error_messages={'required': 'Введите дату задачи'},
                                        widget=forms.DateInput(attrs={'type': 'date'}))
-    description = forms.CharField(label="Описание", widget=forms.Textarea)
+    description = forms.CharField(label="Описание", widget=forms.Textarea, required=False)
 
 # attrs={'type': 'date', 'min': min_day_value, 'value': min_day_value, 'style': 'width: 200px; height: 40px;'}),
 
